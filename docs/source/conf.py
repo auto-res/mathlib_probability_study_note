932ddc21f2b83424e1d69bbc19aa15d6ce87e1a0
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2025-04-04 21:49:20 +0900

    chang conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,38 +2,34 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'mathlib probability study note'
 copyright = '2025, Kei Tsukamoto'
 author = 'Kei Tsukamoto'
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "myst_parser"
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# Theme settings
-html_theme = 'sphinx_rtd_theme'  # Or any other theme you prefer
-
-# Add any Sphinx extension module names here
+# 拡張機能リストを一箇所にまとめる
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'myst_parser'
 ]
+
+# マークダウンファイルの拡張子を指定
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# Theme settings
+html_theme = 'sphinx_rtd_theme' # Or any other theme you prefer
 
 # Output settings for GitHub Pages
 html_static_path = ['_static']
